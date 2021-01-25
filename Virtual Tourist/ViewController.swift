9f7d06abf78cab16af09ac7df9950524d2970584
Roy Fuller <royfull@gmail.com>
2021-01-24 22:20:11 -0500

    Connect mapView outlet, add gesture code

--- Virtual Tourist/ViewController.swift
+++ Virtual Tourist/ViewController.swift
@@ -8,13 +8,36 @@
 import UIKit
 import MapKit
 
-class TravelLocationsMapViewController: UIViewController {
+class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate, UIGestureRecognizerDelegate {
+
+    // MARK: Outlets
+
+    @IBOutlet weak var mapView: MKMapView!
+
+    // MARK: Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.title = "Virtual Tourist"
+        mapView.delegate = self
+
+        // This code was copied from https://knowledge.udacity.com/questions/249077
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleTap(gestureRecognizer:)))
+        gestureRecognizer.delegate = self
+        mapView.addGestureRecognizer(gestureRecognizer)
     }
 
+    // MARK: MapView delegate methods
 
+    // MARK: Gesture recognizer delegate methods
+
+    // The below code was copied from https://knowledge.udacity.com/questions/249077
+    @objc func handleTap(gestureRecognizer: UILongPressGestureRecognizer) {
+        let location = gestureRecognizer.location(in: mapView)
+        let coordinate = mapView.convert(location, toCoordinateFrom: mapView)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        mapView.addAnnotation(annotation)
+    }
 }
 
