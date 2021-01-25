3236498afcfe4ccf48e70e25d23092279212e582
Roy Fuller <royfull@gmail.com>
2021-01-24 22:12:21 -0500

    Initial commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TravelLocationsMapViewController.swift
+//  Virtual Tourist
+//
+//  Created by Roy Fuller on 1/24/21.
+//
+
+import UIKit
+import MapKit
+
+class TravelLocationsMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
