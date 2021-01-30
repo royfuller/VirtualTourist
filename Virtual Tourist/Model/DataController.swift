c05bbff5ed83d94771b11df3d323d75de7baaaf9
Roy Fuller <royfull@gmail.com>
2021-01-30 17:10:03 -0500

    Add DataController

---
+++ Virtual Tourist/Model/DataController.swift
@@ -0,0 +1,59 @@
+//
+//  DataController.swift
+//  Virtual Tourist
+//
+//  Created by Roy Fuller on 1/30/21.
+//
+
+import Foundation
+import CoreData
+
+class DataController {
+    let persistentContainer: NSPersistentContainer
+    
+    var viewContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    var backgroundContext: NSManagedObjectContext!
+    
+    init(modelName: String) {
+        persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func configureContexts() {
+        backgroundContext = persistentContainer.newBackgroundContext()
+        
+        viewContext.automaticallyMergesChangesFromParent = true
+        backgroundContext.automaticallyMergesChangesFromParent = true
+        
+        backgroundContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+        viewContext.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
+    }
+    
+    func load(completion: (() -> Void)? = nil) {
+        persistentContainer.loadPersistentStores { (storeDescription, error) in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+            self.autoSaveViewContext()
+            self.configureContexts()
+            completion?()
+        }
+    }
+}
+
+extension DataController {
+    func autoSaveViewContext(interval: TimeInterval = 30) {
+        guard interval > 0 else {
+            print("cannot set negative autosave interval")
+            return
+        }
+        if viewContext.hasChanges {
+            try? viewContext.save()
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + interval) {
+            self.autoSaveViewContext(interval: interval)
+        }
+    }
+}
