07c3c40e407ff61ae2a5d310d6b8721951269067
Roy Fuller <royfull@gmail.com>
2021-01-30 14:55:45 -0500

    Add appicons, add photo album view, setup pin annotation view in TravelLocationsMapViewController

--- Virtual Tourist/ViewController.swift
+++ Virtual Tourist/ViewController.swift
@@ -28,6 +28,24 @@ class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate, UIG
     }
 
     // MARK: MapView delegate methods
+    
+    // The below code was copied from the PinSample app.
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .infoLight)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
 
     // MARK: Gesture recognizer delegate methods
 
